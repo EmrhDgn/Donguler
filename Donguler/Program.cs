a4d65324c19a47682f44edb558b7dfe071757eef
kizilejder <emrahdogan93@hotmail.com>
2021-03-26 17:27:32 +0300

    Initial commit

---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string meyve1 = "elma";
+            string meyve2 = "armut";
+            string meyve3 = "uzum";
+            //array dizi
+            string[] meyveler = new string[] { "elma", "armut", "uzum", "mandalina", "kiraz" };
+
+
+            for (int i = 0; i < meyveler.Length; i++)
+            {
+                Console.WriteLine(meyveler[i]);
+            }
+            foreach (var meyve in meyveler)
+            {
+                Console.WriteLine(meyve);
+            }
+        }
+    }
+}
